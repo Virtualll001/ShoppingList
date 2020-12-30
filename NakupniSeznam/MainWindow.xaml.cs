c262cbb3ff716ad20bedc7d30784677ee81fba46
Virtualll001 <Virtualll001@gmail.com>
2020-12-30 16:57:02 +0100

    Upload project
    practicing WPF

---
+++ NakupniSeznam/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NakupniSeznam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Organizator organizator;
+        public MainWindow()
+        {
+            InitializeComponent();
+            organizator = new Organizator();
+            try
+            {
+                organizator.Nacti();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Chyba", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            DataContext = organizator;
+        }
+
+        private void pridatButton_Click(object sender, RoutedEventArgs e)
+        {
+            organizator.Pridej(obsahTextBox.Text);
+            obsahTextBox.Text = "";
+            try
+            {
+                organizator.Uloz();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Chyba", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+
+        private void odebratButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (seznamListBox.SelectedItem != null)
+            {
+                organizator.Odeber((string)seznamListBox.SelectedItem);
+                try
+                {
+                    organizator.Uloz();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Chyba", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
---
+++ NakupniSeznam/Organizator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace NakupniSeznam
+{
+    public class Organizator 
+    {
+        private string cesta = "polozky.xml";      
+        public ObservableCollection<string> Polozky { get; set; }
+
+        public Organizator()
+        {
+            Polozky = new ObservableCollection<string>();
+        }        
+        public void Pridej(string polozka)
+        {
+            if (!string.IsNullOrWhiteSpace(polozka))
+            Polozky.Add(polozka);
+        }        
+        public void Odeber(string polozka)
+        {
+            Polozky.Remove(polozka);
+        }
+        public void Nacti()
+        {            
+            if (File.Exists(cesta))
+            {
+                XmlSerializer serializer = new XmlSerializer(Polozky.GetType());
+                using (StreamReader sr = new StreamReader(cesta))
+                {
+                    Polozky = (ObservableCollection<string>)serializer.Deserialize(sr);
+                }
+            }
+            else
+                Polozky = new ObservableCollection<string>();
+        }
+        public void Uloz()
+        {
+            XmlSerializer serializer = new XmlSerializer(Polozky.GetType());
+            using (StreamWriter sw = new StreamWriter(cesta))
+            {
+                serializer.Serialize(sw, Polozky);
+            }
+        }       
+    }
+}
